a449fc4474cce1e22c363182f9936f5e2d684cb1
Hung Nguyen <viethung1996@gmail.com>
2020-02-13 14:26:21 -0500

    Updates

---
+++ precache-manifest.94436aae88c4172a6aa4d97ba59d9c76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efb21af5d68e7b534e346f8210e3486",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "1b79ed06571da5ce11cb",
+    "url": "/todo-app/static/css/main.5b34a09a.chunk.css"
+  },
+  {
+    "revision": "9c13d73ce809ec710265",
+    "url": "/todo-app/static/js/2.99bfbe11.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/todo-app/static/js/2.99bfbe11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b79ed06571da5ce11cb",
+    "url": "/todo-app/static/js/main.59b1a704.chunk.js"
+  },
+  {
+    "revision": "b703beda81a7e762cb66",
+    "url": "/todo-app/static/js/runtime-main.2d89cf30.js"
+  }
+]);
